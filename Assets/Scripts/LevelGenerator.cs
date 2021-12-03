0756382ec1a678573023f3e4cd02b232bf50327c
Ciubix8513 <57328501+Ciubix8513@users.noreply.github.com>
2021-12-03 04:09:47 +0300

    Finished floorplan generation!

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -13,10 +13,10 @@ class QItem
     public int row;
     public int col;
     public int dist;
-   public  QItem(int x, int y, int w)       
+   public  QItem(int rowX, int colY, int w)       
     {
-        row = x;
-        col = y;
+        row = rowX;
+        col = colY;
         dist = w;
     }
 };
@@ -29,12 +29,14 @@ class DistRoom : System.IComparable<int>, System.IComparable<DistRoom>
 {
     public int Distance;
     public int Room;
+    public int[,] pathData;
     public int CompareTo(DistRoom obj) => Distance.CompareTo(obj.Distance);
     int System.IComparable<int>.CompareTo(int other) => Distance.CompareTo(other);
 }
 
 public class LevelGenerator : MonoBehaviour
 {
+    [Header("Debug data")]
     [SerializeField]
     int[] floorplan;
     [SerializeField]
@@ -45,10 +47,19 @@ public class LevelGenerator : MonoBehaviour
     GameObject ItemRoomO;
     [SerializeField]
     GameObject StartRoom;
+
+
+    [Header("Generation settings")]
     [SerializeField]
-    GameObject ShopRoom;
+    [Range(0,99)]
+    int startRoomLocation = 45;
     [SerializeField]
-    GameObject SecretRoomO;
+    [Range(1,8)]
+    int minDistance = 2;
+    [SerializeField]
+    [Range(1, 10)]
+    int maxDistance = 3;
+
 
     void Awake()
     {
@@ -57,8 +68,6 @@ public class LevelGenerator : MonoBehaviour
         GenerateLevel(Random.Range(1, 1000000), SeedSetting.Depth);
         BuildFloor();
     }
-
-
     private void BuildFloor()
     {
         for (int i = 0; i < floorplan.Length; i++)
@@ -85,36 +94,85 @@ public class LevelGenerator : MonoBehaviour
             }
         }
     }
-    int GeneratePath(int org, int dst)
+    bool GeneratePath(int[,] data)
     {
-        return 0;
+        int X, Y;
+        X = startRoomLocation % 10;
+        Y = (startRoomLocation - (startRoomLocation % 10)) / 10;
+        int iter = 0;
+    start:
+        if (iter > 101)
+            return false;
+        // moving right
+        if (X + 1 < 10 &&
+            data[X + 1, Y] < data[X, Y])
+        {
+            X++;
+            floorplan[X + Y * 10] = 1;
+            iter++;
+            goto start;
+        }
+        // moving up
+        if (Y - 1 >= 0 &&
+                data[X , Y - 1] < data[X, Y])
+        {
+            Y--;
+            floorplan[X + Y * 10] = 1;
+            iter++;
+            goto start;
+        }
+        // moving left
+        if (X - 1 >= 0 &&
+            data[X - 1, Y] < data[X, Y])
+        {
+            X--;
+            floorplan[X + Y * 10] = 1;
+            iter++;
+            goto start;
+        }
+
+        // moving down
+        if (Y + 1 < 10 &&
+            data[X , Y + 1] < data[X,Y])
+        {
+            Y++;
+            floorplan[X + Y * 10] = 1;
+            iter++;
+            goto start;
+        }        
+        return true;
     }
+   
 
     //Code *cough* stollen *cough* inspired by code from  https://www.geeksforgeeks.org/shortest-distance-two-cells-matrix-grid/
-    //FIXED I was just being an idiot lol
-    int Distance(int dst, int org)
+    //FIXED! I was just being an idiot lol
+    int Distance(int dst, int org, out int[,] data)
     {
-        QItem source = new QItem(org % 10,( org - (org % 10)) / 10, 0);
-        QItem dst1 = new QItem(dst % 10,( dst - (dst % 10)) / 10, 0);
+        QItem source = new QItem(org % 10, (org - (org % 10)) / 10, 0);
+        QItem dst1 = new QItem(dst % 10, (dst - (dst % 10)) / 10, 0);
 
-        //Debug.Log(dst % 10 + "  " + (dst - (dst % 10)) / 10);
         // To keep track of visited QItems. Marking
         // blocked cells as visited.
         int N = 10;
         int M = 10;
         var visited = new bool[10, 10];
 
-        for (int i = 0; i < N; i++)        
-            for (int j = 0; j < M; j++)    
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
                 if (floorplan[i + j * 10] == 0 || floorplan[i + j * 10] == 2)
                     visited[i, j] = false;
-                else 
-                    visited[i, j] = true; 
+                else
+                    visited[i, j] = true;
 
         // applying BFS on matrix cells starting from source
         Queue<QItem> q = new Queue<QItem>();
         q.Enqueue(source);
         visited[source.row, source.col] = true;
+        data = new int[10, 10];
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)            
+                data[i, j] = int.MaxValue;
+            
         while (q.Count != 0)
         {
             QItem p = q.Dequeue();
@@ -122,13 +180,17 @@ public class LevelGenerator : MonoBehaviour
             // Destination found;
             if (p.row == dst1.row && p.col == dst1.col)
             {
-                return p.dist ;
+                //athData.Add(data);
+                //data[p.row, p.col] = -1;
+                return p.dist;
+
             }
             // moving up
             if (p.row - 1 >= 0 &&
                 visited[p.row - 1, p.col] == false)
             {
                 q.Enqueue(new QItem(p.row - 1, p.col, p.dist + 1));
+                data[p.row - 1, p.col] = p.dist;
                 visited[p.row - 1, p.col] = true;
             }
 
@@ -137,6 +199,7 @@ public class LevelGenerator : MonoBehaviour
                 visited[p.row + 1, p.col] == false)
             {
                 q.Enqueue(new QItem(p.row + 1, p.col, p.dist + 1));
+                data[p.row + 1, p.col] = p.dist;
                 visited[p.row + 1, p.col] = true;
             }
 
@@ -145,6 +208,7 @@ public class LevelGenerator : MonoBehaviour
                 visited[p.row, p.col - 1] == false)
             {
                 q.Enqueue(new QItem(p.row, p.col - 1, p.dist + 1));
+                data[p.row, p.col - 1] = p.dist;
                 visited[p.row, p.col - 1] = true;
             }
 
@@ -153,36 +217,38 @@ public class LevelGenerator : MonoBehaviour
                 visited[p.row, p.col + 1] == false)
             {
                 q.Enqueue(new QItem(p.row, p.col + 1, p.dist + 1));
+                data[p.row, p.col + 1] = p.dist;
                 visited[p.row, p.col + 1] = true;
             }
         }
+
         return -1;
     }
 
     void GenerateLevel(int seed, int depth)
     {
         Random.InitState(seed);
-        var Endrooms = new List<DistRoom>();//;
-                                                    
+        var Endrooms = new List<DistRoom>();
+
         floorplan = new int[100];
         for (int i = 0; i < floorplan.Length; i++) floorplan[i] = 0;
-        floorplan[45] = 2;
+        floorplan[startRoomLocation] = 2;
         //Generate end rooms
         for (int i = 0; i < (depth == 1 ? 0 : 1) + 3; i++)
         {
             var room = Mathf.FloorToInt(Random.value * 100);
-            if (room == 45 || floorplan[room] != 0 ) { i--; continue; }
+            if (room == startRoomLocation || floorplan[room] != 0) { i--; continue; }
             floorplan[room] = 1;
-            //if (i == 0) floorplan[room] = 3; 
-            //else if (i == 1) floorplan[room] = 4; 
-            //else floorplan[room] = 1;
-            var dist = Distance(45, room);
-            Debug.Log(dist);
-            if(dist < 2) { i--; continue; }
-            Endrooms.Add(new DistRoom());
-            Endrooms[i].Room = room;
-            Endrooms[i].Distance = dist;
-            
+            DistRoom dist1 = new DistRoom();
+            var dist = Distance(startRoomLocation, room, out dist1.pathData);
+            if (dist < minDistance || dist > depth + maxDistance) { i--; floorplan[room] = 0; continue; } //Preventing inaccesable rooms and making the less clustered
+            else
+            {
+                Debug.Log(dist);
+                Endrooms.Add(dist1);
+                Endrooms[i].Room = room;
+                Endrooms[i].Distance = dist;
+            }
         }
         Endrooms.Sort();
         //Set farthest room to be a boss room
@@ -190,16 +256,13 @@ public class LevelGenerator : MonoBehaviour
         //Set second farthest room to be an item room
         floorplan[Endrooms[Endrooms.Count - 2].Room] = 4;
         //Make all other normal
-        for (int i = Endrooms.Count - 3; i >= 0; i--)        
+        for (int i = Endrooms.Count - 3; i >= 0; i--)
             floorplan[Endrooms[i].Room] = 1;
-            
-        
-
 
 
         //Generate rooms in between
-        //for (int i = 0; i < Endrooms.Length; i++)        
-        //if (!GeneratePath(Endrooms[i], 45))
+        for (int i = 0; i < Endrooms.Count; i++)
+            GeneratePath(Endrooms[i].pathData);
         return;
     }
 }
